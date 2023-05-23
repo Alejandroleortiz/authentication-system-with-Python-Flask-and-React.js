4039d0e75b060fc73ed97296571d3b0ac72c2a2a
Alejandroleortiz <109121488+Alejandroleortiz@users.noreply.github.com>
2023-05-23 06:50:35 +0000

    terminados endpoints

--- src/api/models.py
+++ src/api/models.py
@@ -5,9 +5,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -16,5 +16,17 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "full_name":self.full_name,
             # do not serialize the password, its a security breach
         }
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
