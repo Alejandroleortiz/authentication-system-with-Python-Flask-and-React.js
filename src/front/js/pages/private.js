cc00e96bed86b7bd29a9f7206d0c3bf271da4a5e
Alejandroleortiz <109121488+Alejandroleortiz@users.noreply.github.com>
2023-05-24 19:16:08 +0000

    finalizado logout

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,11 +1,12 @@
 
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-
-
-import React from 'react'
+import React, { useContext } from 'react'
 
 function Private() {
+
+	const { store, actions } = useContext(Context);
+
 	return (
 		<div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
 			<div className="login text-center">
@@ -15,7 +16,7 @@ function Private() {
 
 					<div className="my-6 ">
 						<small>YOU ARE LOGGED IN! 😎</small>
-
+						<button type='submit' className="btn btn-danger text-uppercase w-100 mt-2" onClick={actions.logout}>logout</button>
 					</div>
 				</div>
 			</div>
