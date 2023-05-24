4657bbb55e4b5df0659430fd1751614d6f9dd4c2
Alejandroleortiz <109121488+Alejandroleortiz@users.noreply.github.com>
2023-05-24 19:29:52 +0000

    finalizado register

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 import { Link, useNavigate } from "react-router-dom";
+import Alert from "../component/alerts/Alert";
 
 
 export const Login = () => {
@@ -14,50 +15,57 @@ export const Login = () => {
 	}, [])
 
 	return (
-		<div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
-			<div className="login">
-				<h2>Login Form</h2>
-				<form className="needs-validation"
-					onSubmit={(e) => {
-						// e.preventDefault();
-						actions.login(e, navigate)
-					}}
-				>
-					<div className="form-group was-validated mb-2">
-						<label htmlFor="email" className="form-label">Email Adress</label>
-						<input type="email" className="form-control" id="email"
-							placeholder="Your email address" required />
-						<div className="invalid-feedback">
-							Please Enter Your Email
+		<>
+			{
+				!!store.error && (
+					<Alert color={"danger"} text={store.error.msg} className={""} />
+				)
+			}
+			<div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
+				<div className="login">
+					<h2>Login Form</h2>
+					<form className="needs-validation"
+						onSubmit={(e) => {
+							// e.preventDefault();
+							actions.login(e, navigate)
+						}}
+					>
+						<div className="form-group was-validated mb-2">
+							<label htmlFor="email" className="form-label">Email Adress</label>
+							<input type="email" className="form-control" id="email"
+								placeholder="Your email address" required />
+							<div className="invalid-feedback">
+								Please Enter Your Email
+							</div>
 						</div>
-					</div>
-					<div className="form-group mb-2 was-validated">
-						<label htmlFor="password" className="form-label">Password</label>
-						<input type="password" className="form-control" id="password"
-							placeholder="Password" required />
-						<div className="invalid-feedback">
-							Please Enter Your Password
+						<div className="form-group mb-2 was-validated">
+							<label htmlFor="password" className="form-label">Password</label>
+							<input type="password" className="form-control" id="password"
+								placeholder="Password" required />
+							<div className="invalid-feedback">
+								Please Enter Your Password
+							</div>
 						</div>
+						<div className="form-group form-check mb-2">
+							<input type="checkbox" className="form-check-input" />
+							<label htmlFor="check" className="form-check-label">Remember me</label>
+						</div>
+						<button type='submit' className="btn btn-success w-100 mt-2">SIGN IN</button>
+					</form>
+					<div className="py-3 text-center">
+						<span className="text-xs text-uppercase fw-bold">or</span>
 					</div>
-					<div className="form-group form-check mb-2">
-						<input type="checkbox" className="form-check-input" />
-						<label htmlFor="check" className="form-check-label">Remember me</label>
-					</div>
-					<button type='submit' className="btn btn-success w-100 mt-2">SIGN IN</button>
-				</form>
-				<div className="py-3 text-center">
-					<span className="text-xs text-uppercase fw-bold">or</span>
-				</div>
-				<div className="row text-center">
+					<div className="row text-center">
 
-					<div className="my-6 ">
-						<small>Don't have an account? </small>
-						<Link to='/register'>
-							Sign up
-						</Link>
+						<div className="my-6 ">
+							<small>Don't have an account? </small>
+							<Link to='/register'>
+								Sign up
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
--- src/front/js/pages/register .js
+++ src/front/js/pages/register .js
@@ -1,33 +1,40 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
 
-export const Register  = () => {
+
+export const Register = () => {
 	const { store, actions } = useContext(Context);
+	const navigate = useNavigate();
+
 
 	return (
 		<div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
 			<div className="login">
 				<h2>Sign Up</h2>
-				<form className="needs-validation">
+				<form className="needs-validation"
+					onSubmit={(e) => {
+						e.preventDefault();
+						actions.register(e, navigate)
+					}}>
 					<div className="form-group was-validated mb-2">
 						<label htmlFor="full_name" className="form-label">Full Name</label>
-						<input type="full_name" className="form-control" required />
+						<input type="full_name" className="form-control" id="full_name" required />
 						<div className="invalid-feedback">
 							Please Enter Your Full Name
 						</div>
 					</div>
 					<div className="form-group was-validated mb-2">
 						<label htmlFor="email" className="form-label">Email Adress</label>
-						<input type="email" className="form-control" required />
+						<input type="email" className="form-control" id="email" required />
 						<div className="invalid-feedback">
 							Please Enter Your Email
 						</div>
 					</div>
 					<div className="form-group mb-2 was-validated">
 						<label htmlFor="password" className="form-label">Password</label>
-						<input type="password" className="form-control" required />
+						<input type="password" className="form-control" id="password" required />
 						<div className="invalid-feedback">
 							Please Enter Your Password
 						</div>
